49a6e1ce67deee402858971bbd618a72f9f98a91
laraj <gralac22@htl-kaindorf.at>
2026-09-23 15:42:17 +0200

    prototype-frontend tests

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import React from "react";
 import { useEffect, useState } from "react";
 
 type Person = {
--- frontend/src/tests/index.test.tsx
+++ frontend/src/tests/index.test.tsx
@@ -8,9 +8,9 @@ import HomeScreen from "../app/index";
 vi.mock("axios");
 
 const people = [
-  { firstname: "Max", lastname: "Muster", email: "max@test.at", age: 25 },
-  { firstname: "Tom", lastname: "Maier", email: "tom@test.at", age: 17 },
-  { firstname: "Anna", lastname: "Huber", email: "anna@test.at", age: 18 },
+  { id: 1, firstName: "Max", lastName: "Muster", email: "max@test.at", age: 25 },
+  { id: 2, firstName: "Tom", lastName: "Maier", email: "tom@test.at", age: 17 },
+  { id: 3, firstName: "Anna", lastName: "Huber", email: "anna@test.at", age: 18 },
 ];
 
 beforeEach(async () => {
@@ -27,30 +27,25 @@ afterEach(() => {
 describe("HomeScreen", () => {
   it("lädt und zeigt die Personen", () => {
     expect(axios.get).toHaveBeenCalledWith("http://localhost:8080/vk/api");
-    expect(screen.getAllByText(/\(\d+\)$/).map((x) => x.textContent)).toEqual([
-      "Max Muster (25)",
-      "Tom Maier (17)",
-      "Anna Huber (18)",
-    ]);
+    expect(
+        screen.getAllByText(/\(\d+\)$/).map((x: HTMLElement) => x.textContent)
+    ).toEqual(["Max Muster (25)", "Tom Maier (17)", "Anna Huber (18)"]);
   });
 
   it("sortiert nach Alter", () => {
     fireEvent.click(screen.getByRole("button", { name: "Sort People" }));
 
-    expect(screen.getAllByText(/\(\d+\)$/).map((x) => x.textContent)).toEqual([
-      "Tom Maier (17)",
-      "Anna Huber (18)",
-      "Max Muster (25)",
-    ]);
+    expect(
+        screen.getAllByText(/\(\d+\)$/).map((x: HTMLElement) => x.textContent)
+    ).toEqual(["Tom Maier (17)", "Anna Huber (18)", "Max Muster (25)"]);
   });
 
   it("filtert Personen unter 18 heraus", () => {
     fireEvent.click(screen.getByRole("button", { name: "Filter ü. 18" }));
 
     expect(screen.queryByText("Tom Maier (17)")).toBeNull();
-    expect(screen.getAllByText(/\(\d+\)$/).map((x) => x.textContent)).toEqual([
-      "Max Muster (25)",
-      "Anna Huber (18)",
-    ]);
+    expect(
+        screen.getAllByText(/\(\d+\)$/).map((x: HTMLElement) => x.textContent)
+    ).toEqual(["Max Muster (25)", "Anna Huber (18)"]);
   });
 });
